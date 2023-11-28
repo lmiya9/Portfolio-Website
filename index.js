d527e683adb28b078f92b10de2ab4f5bf289be36
lmiya9 <114008971+lmiya9@users.noreply.github.com>
2023-11-27 16:49:02 -0800

    removed JS for scrolling

--- index.js
+++ index.js
@@ -1,16 +1,16 @@
 
 
-  window.addEventListener("scroll", function () {
-    let skillsContainer = document.querySelector(".skills-container");
-    let heroSection = document.querySelector("#scroll");
+  // window.addEventListener("scroll", function () {
+  //   let skillsContainer = document.querySelector(".skills-container");
+  //   let heroSection = document.querySelector("#scroll");
 
     
-    let heroSectionHeight = heroSection.clientHeight;
+  //   let heroSectionHeight = heroSection.clientHeight;
 
     
-    if (window.scrollY > heroSectionHeight) {
-      skillsContainer.classList.add("scrolled");
-    } else {
-      skillsContainer.classList.remove("scrolled");
-    }
-  });
+  //   if (window.scrollY > heroSectionHeight) {
+  //     skillsContainer.classList.add("scrolled");
+  //   } else {
+  //     skillsContainer.classList.remove("scrolled");
+  //   }
+  // });
